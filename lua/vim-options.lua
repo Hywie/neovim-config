f7b7db4c841efa5c231e979d718486fd58a07eef
Hywel Martin <hywie@Hywels-MacBook-Pro.local>
2025-01-13 08:57:32 +0000

    Add line options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,6 +1,15 @@
+-- Helpful vars
+local opt = vim.opt
+
+-- tab options
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+-- line numbers
+opt.relativenumber = true -- show relative line numbers
+opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
